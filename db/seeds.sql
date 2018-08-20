2e74182973c9a5757beb1edba20b7758fb952d57
AndyJ <andrewja01@gmail.com>
2018-08-19 17:54:50 -0700

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Double Cheeseburger', TRUE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Baconator', TRUE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Big Texas', FALSE);
